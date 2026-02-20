1fc562c52938ac41d7787a413abdf5483483613c
Jayavandhini <jayavandhini.kuttybabu@gmail.com>
2026-02-20 11:33:11 +0530

    hosting process

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://sawaari-09bb.onrender.com';
 
 const api = axios.create({
     baseURL: `${BASE_URL}/api`,
